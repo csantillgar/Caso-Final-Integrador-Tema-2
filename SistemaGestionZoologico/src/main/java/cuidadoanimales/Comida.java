6d40b07c1cedeea77f0d12eef78f30eb42710c98
csantillgar <csantill@myuax.com>
2024-03-11 21:09:56 +0100

    guardar

---
+++ SistemaGestionZoologico/src/main/java/cuidadoanimales/Comida.java
@@ -0,0 +1,19 @@
+package cuidadoanimales;
+
+public class Comida {
+    private String nombre;
+    private String tipo;
+
+    public Comida(String nombre, String tipo) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    public void servir() {
+        System.out.println("Sirviendo comida: " + nombre);
+    }
+
+    public void preparar() {
+        System.out.println("Preparando comida: " + nombre);
+    }
+}
